fd4cf02f4dc123c8525e7e5897fb2a999e3b1625
Your Name <your@email.com>
2026-01-29 17:09:25 +0530

    added dsa practice code

---
+++ day2.py/dsaday2.py
@@ -0,0 +1,20 @@
+import array as arr
+arrayName = arr.array("i",[23,87,90])
+
+#1.add
+#a)apend (value)-> add the values in last ,t/c - 0(1),0(n)
+#b) insert(index,value)-> insert the value at index, t/c-0(1),0(n)
+#2.delete
+#1.remove()
+#2.pop() or pop(index)
+
+arrayName.remove(87)
+print(arrayName)
+
+arrayName.pop(1)
+print(arrayName)
+arrayName[0]=40
+print(arrayName)
+
+for i in range(0,len(arrayName)):
+    print(arrayName[1]),end="")
---
+++ day2.py/practicequestiondsa.py
@@ -0,0 +1,20 @@
+import array as arr
+arrayName=arr.array("i",[12,35,56,67])
+for i in range(0,len(arrayName),2):#time complexity 0(n)
+    print(arrayName[1],end=" ")
+
+#write a program to get a pro
+    sum=0
+    product=1
+    for i in range(0,len(arrayName)):
+        sum=sum+arrayName[i]
+        product=product * arrayName[i]
+        print ("sum is:",sum)
+        print ("product is:",product)
+   
+ #write program to count target value in array
+    count=0
+    target=int(input("enter target vlue"))
+    
+
+ 
